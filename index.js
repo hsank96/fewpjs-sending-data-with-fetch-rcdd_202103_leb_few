5e3c68787f20f7f0ba77b08396fa23d00a574c7a
Hazem El Sankari <hazemleb96@gmail.com>
2021-05-06 07:31:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
-// Add your code here
+// // Add your code here
+// fetch("http://localhost:3000/dogs", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   })
+// });
+
+// function submitData(userName,userEmail){
+//   return fetch("http://localhost:3000/users",
+//   {
+//     method: "POST",
+//     headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify({
+//     name: `${userName}`,
+//     email: `${userEmail}`
+//   })
+// })
+//   .then(function(response){
+//     return response.json()
+//   })
+//   .then(function(object){
+//     console.log(object)
+//   })
+//   .catch(function(error){
+//     alert("Error!");
+//     console.log(error.message)
+//   })
+// }
+
+// console.log(submitData())
+
+
+function submitData(n,e){
+  return fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    name: n,
+    email: e
+  })
+})
+ .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+     document.body.innerHTML = object[ "id" ]
+    console.log(object);
+  })
+  .catch(function(error) {
+    document.body.innerHTML = error.message
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  });
+}
